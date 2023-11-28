3d691b589f32a10bbfd404c03c1e4672d8171e4a
jihan Nabilah <133481019+jihanabilah07@users.noreply.github.com>
2023-11-29 02:17:18 +0700

    Update Barang.java
    nambahin setter getter aja

--- Barang.java
+++ Barang.java
@@ -3,5 +3,45 @@ public class Barang {
   private String namaBarang;
   private int hargaBarang;
   private int stokBarang;
+
+   // Setter untuk idBarang
+    public void setIdBarang(int idBarang) {
+        this.idBarang = idBarang;
+    }
+
+    // Getter untuk idBarang
+    public int getIdBarang() {
+        return idBarang;
+    }
+
+    // Setter untuk namaBarang
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    // Getter untuk namaBarang
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    // Setter untuk hargaBarang
+    public void setHargaBarang(int hargaBarang) {
+        this.hargaBarang = hargaBarang;
+    }
+
+    // Getter untuk hargaBarang
+    public int getHargaBarang() {
+        return hargaBarang;
+    }
+
+    // Setter untuk stokBarang
+    public void setStokBarang(int stokBarang) {
+        this.stokBarang = stokBarang;
+    }
+
+    // Getter untuk stokBarang
+    public int getStokBarang() {
+        return stokBarang;
+    }
 }  
   
