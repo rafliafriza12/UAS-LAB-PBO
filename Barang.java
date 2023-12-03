db9099fa7a57b3c8457a711d791b2cc1bcc6b5c6
jihan Nabilah <133481019+jihanabilah07@users.noreply.github.com>
2023-12-03 22:19:48 +0700

    Update Barang.java

--- Barang.java
+++ Barang.java
@@ -1,47 +1,88 @@
-public class Barang {
-  private int idBarang;
-  private String namaBarang;
-  private int hargaBarang;
-  private int stokBarang;
+/**
+ * Kelas Barang merepresentasikan informasi tentang suatu barang dalam sistem inventaris.
+ * Kelas ini memiliki atribut untuk mengatur ID barang, nama barang, harga barang, dan stok.
+ */
+public class Barang extends ListBarang {
+    private int idBarang;       // Menyimpan ID barang
+    private String namaBarang;  // Menyimpan nama barang
+    private int hargaBarang;    // Menyimpan harga barang
+    private int stok;           // Menyimpan jumlah stok barang
 
-   // Setter untuk idBarang
+    /**
+     * Konstruktor default untuk kelas Barang.
+     * Tidak melakukan operasi inisialisasi.
+     */
+    public Barang() {
+        
+    }
+
+    // Start method setter
+
+    /**
+     * Setter untuk mengatur ID barang.
+     * @param idBarang ID yang akan diatur untuk barang.
+     */
     public void setIdBarang(int idBarang) {
         this.idBarang = idBarang;
     }
 
-    // Getter untuk idBarang
-    public int getIdBarang() {
-        return idBarang;
-    }
-
-    // Setter untuk namaBarang
+    /**
+     * Setter untuk mengatur nama barang.
+     * @param namaBarang Nama yang akan diatur untuk barang.
+     */
     public void setNamaBarang(String namaBarang) {
         this.namaBarang = namaBarang;
     }
 
-    // Getter untuk namaBarang
-    public String getNamaBarang() {
-        return namaBarang;
-    }
-
-    // Setter untuk hargaBarang
+    /**
+     * Setter untuk mengatur harga barang.
+     * @param hargaBarang Harga yang akan diatur untuk barang.
+     */
     public void setHargaBarang(int hargaBarang) {
         this.hargaBarang = hargaBarang;
     }
 
-    // Getter untuk hargaBarang
+    /**
+     * Setter untuk mengatur stok barang.
+     * @param stok Jumlah stok yang akan diatur untuk barang.
+     */
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+    // End of method setter
+
+    // Start method getter
+
+    /**
+     * Getter untuk mendapatkan ID barang.
+     * @return ID barang.
+     */
+    public int getIdBarang() {
+        return this.idBarang;
+    }
+
+    /**
+     * Getter untuk mendapatkan nama barang.
+     * @return Nama barang.
+     */
+    public String getNamaBarang() {
+        return this.namaBarang;
+    }
+
+    /**
+     * Getter untuk mendapatkan harga barang.
+     * @return Harga barang.
+     */
     public int getHargaBarang() {
-        return hargaBarang;
+        return this.hargaBarang;
     }
 
-    // Setter untuk stokBarang
-    public void setStokBarang(int stokBarang) {
-        this.stokBarang = stokBarang;
+    /**
+     * Getter untuk mendapatkan stok barang.
+     * @return Jumlah stok barang.
+     */
+    public int getStok() {
+        return this.stok;
     }
-
-    // Getter untuk stokBarang
-    public int getStokBarang() {
-        return stokBarang;
-    }
-}  
-  
+    // End of method getter
+}
